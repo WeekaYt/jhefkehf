6bd104f3db718960c342dd9de274217fc79f6b1b
WeekaYt <51525334+WeekaYt@users.noreply.github.com>
2019-06-12 18:40:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,8 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== ""NDA5MzgxMTYzOTgzOTYyMTEy.XP-u9g.92hTZEZiR9mNnz7LvdIeqjzSdWc"
+") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
